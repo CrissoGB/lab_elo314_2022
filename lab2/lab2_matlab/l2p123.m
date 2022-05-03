9d8d79f9986d14dbcccb0f80b5985eca56e244e2
CrissoGB <62680224+CrissoGB@users.noreply.github.com>
2022-05-03 04:00:32 -0400

    archivo biquad y script multitap delay

--- lab2/lab2_matlab/l2p123.m
+++ lab2/lab2_matlab/l2p123.m
@@ -1,18 +1,27 @@
 %% Parte 2.2
 
 [x, Fs] = audioread('gtr-jazz_16_48.wav');
+x = x(:,1);     % un solo canal
 
-x = x(:,1); % un solo canal
-N = 4;
-s = 0.125;  % longitud de cada retardo en segundos
-M = Fs*s;   % longitud de cada retardo en muestras
-bk = 0.35;
+xgd = x;        % señal de entrada
+xgd = resample(xgd, 16, 48); Fs = 1/3*Fs;
 
-buffer = zeros(1,N*M);
-y = zeros(length(x),1);
+N = 4;          % numero de etapas de retardo
+l = 0.250;      % longitud en s de la etapa de retardo.
+b_k = 0.35;     % ganancia por etapa de retardo
+M = l*Fs;       % # de muestras equivalentes a la longitud de cada retardo
 
-for i = 1:length(x)
-    for j = 1:N
-        y(i) = y(i) + bk*buffer(j*M);
+gtdelay = zeros(size(xgd)); % salida
+
+for i = 1:length(xgd)
+    dval_aux = 0;
+    for k = 1:N
+       if(i-k*M>0)      % asegura causalidad (y evita errores)
+           dval_aux = dval_aux + b_k^k*xgd(i-k*M); 
+           % ganancia: variable -> b_k^k | constante -> b_k
+       else
+           break
+       end
     end
+    gtdelay(i) = xgd(i) + dval_aux;
 end
