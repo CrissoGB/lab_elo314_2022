08a2f56f9e9e4d4817854fdf72b4f77366eba233
Joaquín Videla <56414062+skrvidz@users.noreply.github.com>
2022-07-19 17:29:15 -0400

    Update 19/07

--- lab5/lab5p1.m
+++ lab5/lab5p1.m
@@ -191,111 +191,231 @@ load("test_training_signals.mat");
 
 x1 = test_signal;
 x2 = training_signal;
-%soundsc(x1,fs)
+soundsc(x2,fs)
 L=length(x1);
 
 t = 0:1/fs:L/fs-1/fs;
-%%
+% [u1,u2] = segment(x2);
 
-audiowrite("entrenamiento.wav",x2,fs)
+%% Segmentos SUV 
 
-%%
-
-% Segmentación de silencios
-%[S11, S12] = segment(x1);
-%[S21, S22] = segment(x1);
-%[S31, S32] = segment(x1);
-
-% Segmentación sin vibración de cuerda vocales. U
-
-%[V11, V12] = segment(x1);
-%[V21, V22] = segment(x1);
-%[V31, V32] = segment(x1);
-%[V41, V42] = segment(x1);
-%[V51, V52] = segment(x1);
-
-%%
-S1 = x1(1:1675);
-U1 = x1(1675:1937);
-V1 = x1(1937:2242);
-U2 = x1(2242:3077);
-V2 = x1(3077:3887);
-U3 = x1(3887:5600);
-V3 = x1(5600:6003);
-S2 = x1(6003:6614);
-U4 = x1(6614:7241);
-V4 = x1(7241:8373);
-U5 = x1(8373:8758);
-V5 = x1(8758:9584);
-S3 = x1(9584:end);
-
+S1 = 1:2000;
+U1 = 2000:3060;
+V1 = 3060:3600;
+U2 = 3600:4150;
+V2 = 4150:5400;
+U3 = 5400:5800;
+V3 = 5800:6690;
+U4 = 6690:7830;
+S2 = 7830:8400;
+U5 = 8400:8700;
+V4 = 8700:9200;
+U6 = 9200:10200;
+U7 = 10200:10500;
+V5 = 10500:10800;
+U8 = 10800:11000;
+V6 = 11000:11900;
+U9 = 11900:12500;
+V7 = 12500:13400;
+U10 = 13400:15450;
+S3 = 15450:15920;
+%% PLOT de los segmentos VUS (manual)
 figure
-plot(t(1:1675),S1,'black'), hold on % SILENCIO 1
-plot(t(1675:1937),U1,'green') % CONSONANTE S
-plot(t(1937:2242),V1,'magenta') % VOCAL E
-plot(t(2242:3077),U2,'green') % CONSONANTE Ñ
-plot(t(3077:3887),V2,'magenta') % VOCAL A 
-plot(t(3887:5600),U3,'green') % CONSONANTE L + CONSONANTE D
-plot(t(5600:6003),V3,'magenta') % VOCAL E
-plot(t(6003:6614),S2,'black') % SILENCIO 2
-plot(t(6614:7241),U4,'green') % CONSONANTE PR
-plot(t(7241:8373),V4,'magenta') % VOCALES UE
-plot(t(8373:8758),U5,'green') % CONSONANTE B
-plot(t(8758:9584),V5,'magenta') % VOCAL A
-plot(t(9584:end),S3,'black') % SILENCIO 3
+plot(t(S1),x2(S1),'black'), hold on % SILENCIOS
+plot(t(S2),x2(S2),'black')
+plot(t(S3),x2(S3),'black')
+
+plot(t(U1),x2(U1),'green') % CONSONANTES
+plot(t(U2),x2(U2),'green')
+plot(t(U3),x2(U3),'green')
+plot(t(U4),x2(U4),'green')
+plot(t(U5),x2(U5),'green')
+plot(t(U6),x2(U6),'green')
+plot(t(U7),x2(U7),'green')
+plot(t(U8),x2(U8),'green')
+plot(t(U9),x2(U9),'green')
+plot(t(U10),x2(U10),'green')
+
+plot(t(V1),x2(V1),'magenta') % VOCALES
+plot(t(V2),x2(V2),'magenta')
+plot(t(V3),x2(V3),'magenta')
+plot(t(V4),x2(V4),'magenta')
+plot(t(V5),x2(V5),'magenta')
+plot(t(V6),x2(V6),'magenta')
+plot(t(V7),x2(V7),'magenta')
 
 grid on 
+hold off 
 %% Calculo de Vrms y Zero crossing count
 
+zcS1 = zerocrossing(x2(S1));
+zcS2 = zerocrossing(x2(S2));
+zcS3 = zerocrossing(x2(S3));
 
-zcS1 = zerocrossing(S1);
-zcS2 = zerocrossing(S2);
-zcS3 = zerocrossing(S3);
+zcU1 = zerocrossing(x2(U1));
+zcU2 = zerocrossing(x2(U2));
+zcU3 = zerocrossing(x2(U3));
+zcU4 = zerocrossing(x2(U4));
+zcU5 = zerocrossing(x2(U5));
+zcU6 = zerocrossing(x2(U6));
+zcU7 = zerocrossing(x2(U7));
+zcU8 = zerocrossing(x2(U8));
+zcU9 = zerocrossing(x2(U9));
+zcU10 = zerocrossing(x2(U10));
 
-zcU1 = zerocrossing(U1);
-zcU2 = zerocrossing(U2);
-zcU3 = zerocrossing(U3);
-zcU4 = zerocrossing(U4);
-zcU5 = zerocrossing(U5);
+zcV1 = zerocrossing(x2(V1));
+zcV2 = zerocrossing(x2(V2));
+zcV3 = zerocrossing(x2(V3));
+zcV4 = zerocrossing(x2(V4));
+zcV5 = zerocrossing(x2(V5));
+zcV6 = zerocrossing(x2(V6));
+zcV7 = zerocrossing(x2(V7));
 
-zcV1 = zerocrossing(V1);
-zcV2 = zerocrossing(V2);
-zcV3 = zerocrossing(V3);
-zcV4 = zerocrossing(V4);
-zcV5 = zerocrossing(V5);
 
-S1rms = rms(S1);
-S2rms = rms(S2);
-S3rms = rms(S3);
+S1rms = rms(x2(S1));
+S2rms = rms(x2(S2));
+S3rms = rms(x2(S3));
 
-U1rms = rms(U1);
-U2rms = rms(U2);
-U3rms = rms(U3);
-U4rms = rms(U4);
-U5rms = rms(U5);
 
-V1rms = rms(V1);
-V2rms = rms(V2);
-V3rms = rms(V3);
-V4rms = rms(V4);
-V5rms = rms(V5);
+U1rms = rms(x2(U1));
+U2rms = rms(x2(U2));
+U3rms = rms(x2(U3));
+U4rms = rms(x2(U4));
+U5rms = rms(x2(U5));
+U6rms = rms(x2(U6));
+U7rms = rms(x2(U7));
+U8rms = rms(x2(U8));
+U9rms = rms(x2(U9));
+U10rms = rms(x2(U10));
 
+V1rms = rms(x2(V1));
+V2rms = rms(x2(V2));
+V3rms = rms(x2(V3));
+V4rms = rms(x2(V4));
+V5rms = rms(x2(V5));
+V6rms = rms(x2(V6));
+V7rms = rms(x2(V7));
+
+figure()
 scatter(V1rms,zcV1,'magenta')
 hold on 
 scatter(V2rms,zcV2,'magenta')
 scatter(V3rms,zcV3,'magenta')
 scatter(V4rms,zcV4,'magenta')
 scatter(V5rms,zcV5,'magenta')
+scatter(V6rms,zcV6,'magenta')
+scatter(V7rms,zcV7,'magenta')
 
 scatter(U1rms,zcU1,'blue')
 scatter(U2rms,zcU2,'blue')
 scatter(U3rms,zcU3,'blue')
 scatter(U4rms,zcU4,'blue')
 scatter(U5rms,zcU5,'blue')
+scatter(U6rms,zcU6,'blue')
+scatter(U7rms,zcU7,'blue')
+scatter(U8rms,zcU8,'blue')
+scatter(U9rms,zcU9,'blue')
+scatter(U10rms,zcU10,'blue')
 
 scatter(S1rms,zcS1,'black')
 scatter(S2rms,zcS2,'black')
 scatter(S3rms,zcS3,'black')
+%%
+% umbral rms SILENCIO
+s_th = 0.08;
+
+% umbral rms U/V 
+uv_th = 0.040; uv_vzc = 180;
+
+% umbral rms V 
+v_th = 0.0725;
+%% II.2) 
+
+l = length(x1); 
+frame_idx = 1:l/20; % lotes de muestras.
+
+VUS = zeros(length(frame_idx),1); % Salida VUS (1,-1,0).
+RMS = zeros(length(frame_idx),1);
+
+for k = 1:length(frame_idx)
+    frame = x1((20*(k-1)+1):20*(k)); % Muestreado en frames.
+    rms_v = rms(frame);   % RMS del frame de 20 muestras.
+    zc_v = zerocrossing(frame); % Cruces por cero del frame.
+    RMS(k) = rms_v;
+    if (rms_v < s_th)   % Umbral de rms para determinar Silencio.
+        VUS(k) = 0;
+    elseif (rms_v>s_th && rms_v<uv_th)
+        VUS(k) = -1;
+    elseif (rms_v < uv_th && zc_v <200)  % Zona de incerteza U/V. 
+        VUS(k) = -1;
+    elseif (rms_v < uv_th && zc_v > 200)
+        VUS(k) = 1;
+    else                            % Umbral rms para determinar vocales V.
+        VUS(k) = 1;
+    end
+end
+figure()
+subplot(3,1,1)
+plot(x1)
+subplot(3,1,2)
+stairs(VUS)
+subplot(3,1,3)
+plot(RMS)
+
+%% III)
+
+load("test_training_signals.mat");
+x = training_signal;
+N = length(x);
+t = 0:1/fs:N/fs-1/fs;
+fo = 100; %frecuencia fundamental Hz
+T = (1/fo); % periodo en segundos
+Np = round(fs*T); % periodo en muestras del tren de impulsos
+
+frame_idx = 1:N/20; % lotes de muestras.
+
+VUS = zeros(length(frame_idx),1); % Salida VUS (1,-1,0).
+RMS = zeros(length(frame_idx),1);
+Acoeff = zeros(length(frame_idx),16);
+
+for k = frame_idx
+    frame = x((20*(k-1)+1):20*(k)); % Muestreado en frames.
+    rms_v = rms(frame);   % RMS del frame de 20 muestras.
+    zc_v = zerocrossing(frame); % Cruces por cero del frame.
+    RMS(k) = rms_v;
+    Acoeff(k,:) = mylpc2(frame,15);
+    if (rms_v < s_th)   % Umbral de rms para determinar Silencio.
+        VUS(k) = 0;
+    elseif (rms_v < uv_th && zc_v <200)  % Zona de incerteza U/V. 
+        VUS(k) = -1;
+    elseif (rms_v < uv_th && zc_v > 200)
+        VUS(k) = 1;
+    else                            % Umbral rms para determinar vocales V.
+        VUS(k) = 1;
+    end
+end
+
+N = 20;
+X = exciteV(20,Np);
+X1 = rand(1,20);
+
+synth_frame = zeros(length(frame_idx),20);
+
+for k = frame_idx
+    if VUS(k) ==1
+        synth_frame(k,:) = filter(1,Acoeff(k,:),X);
+    elseif VUS(k) ==-1
+        synth_frame(k,:) = filter(1,Acoeff(k,:),X1);
+    else 
+        synth_frame(k,:) = zeros(N,1);
+    end
+end
+
+synth_audio = zeros(N*length(frame_idx),1);
+
+for k = frame_idx
+        synth_audio(20*(k-1)+1:k*20)=synth_frame(k,:);
+end
 
 %%
 function zc = zerocrossing(x)
@@ -312,6 +432,30 @@ end
 function y = mylpc(x,P)
     N = 160; % frame length
     i = 50; % lote n°i de N muestras
+    sp = i*N+1; %starting point (sample)
+
+    %vector de N muestras de x(n)
+    x = x(sp:sp+N-1);
+
+    % autocorrelación del vector x con desfase máximo P.
+    rx = xcorr(x,P); % largo 2*P + 1
+
+    % matriz Rx simétrica con toeplitz
+    RxColumn = rx(P+1:end-1); % largo P rx(0:P-1)
+    Rx = toeplitz(RxColumn);
+
+    % vector xcorr corregido 
+    rx2 = rx(P+2:end); % largo P rx(1:P)
+
+    % Rx*a = rx => Rx^-1*Rx*a rx*Rx^-1 => a = Rx^-1* rx
+    ag = Rx\rx2;
+    a = [1 -ag.'];
+    y = a; 
+end
+
+function y = mylpc2(x,P)
+    N = 20; % frame length
+    i = 0; % lote n°i de N muestras
     sp = i*N+1; %starting point (sample)
 
     %vector de N muestras de x(n)
